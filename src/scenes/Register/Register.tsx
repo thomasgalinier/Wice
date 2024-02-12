a4611c372ff345c006eea3d7ac012d22c53765be
thomas.galinier <thomas.galinier@braincube.com>
2024-02-12 10:30:31 +0100

    progress: authfront connection

--- src/scenes/Register/Register.tsx
+++ src/scenes/Register/Register.tsx
@@ -7,7 +7,8 @@ function Register() {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
-
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
     const email = formData.get("email");
     const password = formData.get("password");
 
@@ -17,16 +18,19 @@ function Register() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        firstname: firstName,
+        lastname: lastName,
         email: email,
         password: password,
+        iconurl: `https://api.dicebear.com/7.x/micah/svg?seed=${email}`,
+        accesstype:"user"
       }),
     };
 
-    fetch("http://localhost:3000/auth/signin", requestOptions)
+    fetch("http://localhost:3000/auth/signup", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         navigate("/");
-        // 'expires' définit la durée de vie du cookie en jours
         console.log(result);
       })
       .catch((error) => console.error(error));
@@ -34,45 +38,45 @@ function Register() {
     form.reset();
   };
   return (
-      <section className="auth-container">
-        <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label htmlFor="firstName">Prénom</label>
-            <input
-              name="firstName"
-              type="text"
-              id="firstName"
-              placeholder="Prénom"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="lastName">Nom</label>
-            <input
-              name="lastName"
-              type="text"
-              id="lastName"
-              placeholder="Nom"
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="email">Email</label>
-            <input name="email" type="email" id="email" placeholder="email" />
-          </div>
-          <div className="input-container">
-            <label htmlFor="password">Mot de passe</label>
-            <input
-              name="password"
-              type="password"
-              id="password"
-              placeholder="mot de passe"
-            />
-          </div>
-          <button type="submit" className="primary">
-            Inscrivez-vous
-          </button>
-        </form>
-        <img src={authIllustration} alt="secure login" />
-      </section>
+    <section className="auth-container">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="firstName">Prénom</label>
+          <input
+            name="firstName"
+            type="text"
+            id="firstName"
+            placeholder="Prénom"
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="lastName">Nom</label>
+          <input
+            name="lastName"
+            type="text"
+            id="lastName"
+            placeholder="Nom"
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="email">Email</label>
+          <input name="email" type="email" id="email" placeholder="email" />
+        </div>
+        <div className="input-container">
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            name="password"
+            type="password"
+            id="password"
+            placeholder="mot de passe"
+          />
+        </div>
+        <button type="submit" className="primary">
+          Inscrivez-vous
+        </button>
+      </form>
+      <img src={authIllustration} alt="secure login" />
+    </section>
   );
 }
 
